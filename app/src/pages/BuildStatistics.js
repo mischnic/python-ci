c0ea1305e9aea43983f663a2e1b3a12831ad533d
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2017-12-09 12:13:38 +0100

    Stats: option for time as x axis (#90)

--- app/src/pages/BuildStatistics.js
+++ app/src/pages/BuildStatistics.js
@@ -12,10 +12,21 @@ export default class BuildStatistics extends React.Component {
 		super(props);
 
 		this.state = {
-			hide: Settings.get("stats.hide")
+			hide: Settings.get("stats.hide"),
+			xType: Settings.get("stats.xType")
 		};
 
+		this.data = {};
+
 		this.legendClick = this.legendClick.bind(this);
+		this.typeChange = this.typeChange.bind(this);
+		this.label = this.label.bind(this);
+		this.labelTooltip = this.labelTooltip.bind(this);
+	}
+
+	typeChange(e){
+		const v = e.target.value;
+		this.setState({xType: v}, ()=>Settings.set(`stats.xType`, v))
 	}
 
 	legendClick(e){
@@ -29,55 +40,104 @@ export default class BuildStatistics extends React.Component {
 				}
 			}
 		};
-		this.setState(v, Settings.set(`stats.hide`, v.hide))
+		this.setState(v, ()=>Settings.set(`stats.hide`, v.hide))
+	}
+
+	label(t){
+		const d = new Date(t);
+		return `${String(d.getFullYear()).slice(-2)}/${d.getMonth()+1}/${d.getDate()}`;
+	}
+
+	labelTooltip(t){
+		const ref = this.data[t];
+		return ref ? ref.slice(0,7) : null;
 	}
 
 	render(){
 		const {proj} = this.props.match.params;
 		const {language, list} = this.props.info.data;
-		const hide = this.state.hide[proj] || {};
+		const hide = this.state.hide ? (this.state.hide[proj] || {}) : {};
 		let diagram = null;
 		if(language === "latex"){
+			this.data = {};
 			const data = list.map((v)=>{
-										if(v.build.status === "success" && v.build.stats.counts){
-											return {
-												date: `${String(v.commit.date.getFullYear()).slice(-2)}/${v.commit.date.getMonth()+1}/${v.commit.date.getDate()} ${v.commit.ref.slice(0,7)}`,
-												// ref: v.commit.ref,
-												Words: +v.build.stats.counts.words.total[0],
-												Letters: +v.build.stats.counts.letters.total[0],
-												"Build Time": Math.round(v.build.duration)
+										if(this.state.xType === "time"){
+											this.data[v.commit.date.getTime()] = v.commit.ref;
+											if(v.build.status === "success" && v.build.stats.counts){
+													return {
+														date: v.commit.date.getTime(),
+														ref: v.commit.ref,
+														words: +v.build.stats.counts.words.total[0],
+														letters: +v.build.stats.counts.letters.total[0],
+														buildTime: Math.round(v.build.duration)
+													}
+											} else {
+												return {
+													date: v.commit.date.getTime()
+												}
 											}
 										} else {
-											return {
-												date: `${String(v.commit.date.getFullYear()).slice(-2)}/${v.commit.date.getMonth()+1}/${v.commit.date.getDate()} ${v.commit.ref.slice(0,7)}`,
+											if(v.build.status === "success" && v.build.stats.counts){
+													return {
+														date: `${String(v.commit.date.getFullYear()).slice(-2)}/${v.commit.date.getMonth()+1}/${v.commit.date.getDate()} ${v.commit.ref.slice(0,7)}`,
+														words: +v.build.stats.counts.words.total[0],
+														letters: +v.build.stats.counts.letters.total[0],
+														buildTime: Math.round(v.build.duration)
+													}
+											} else {
+												return {
+													date: `${String(v.commit.date.getFullYear()).slice(-2)}/${v.commit.date.getMonth()+1}/${v.commit.date.getDate()} ${v.commit.ref.slice(0,7)}`,
+												}
 											}
 										}
 									}).reverse();
+
 			diagram = <LineChart data={data}>
-							<XAxis dataKey="date"/>
+							{ this.state.xType === "time" ?
+								<XAxis dataKey="date"
+									domain = {['dataMin', 'dataMax']}
+									name = 'Time'
+									tickFormatter = {this.label}
+									type = 'number'/>
+									:
+								<XAxis dataKey="date" />
+							}
+
 							<YAxis label={{ value: 'Count', angle: -90, position: 'left', offset: -5 }} 
 									orientation="left"
 									yAxisId="counts"/>
 							<YAxis label={{ value: 'Build Time (sec)', angle: -90, position: 'insideRight', offset: 10 }}
 									orientation="right"
 									yAxisId="time"/>
-							<Tooltip/>
+							{ this.state.xType === "time" ?
+								<Tooltip labelFormatter={this.labelTooltip}/>
+								:
+								<Tooltip />
+							}
 							<Legend onClick={this.legendClick}/>
-							<Line hide={hide["Words"]}      dataKey="Words"      type="monotone" strokeWidth="2" stroke="#8884d8" yAxisId="counts" animationDuration={200}/>
-							<Line hide={hide["Letters"]}    dataKey="Letters"    type="monotone" strokeWidth="2" stroke="#82ca9d" yAxisId="counts" animationDuration={200}/>
-							<Line hide={hide["Build-Time"]} dataKey="Build Time" type="monotone" strokeWidth="2" stroke="#ff7f0e" yAxisId="time"   animationDuration={200}/>
+							<Line hide={hide["words"]}     dataKey="words"     name="Words"      type="monotone" strokeWidth="2" stroke="#8884d8" yAxisId="counts" animationDuration={200}/>
+							<Line hide={hide["letters"]}   dataKey="letters"   name="Letters"    type="monotone" strokeWidth="2" stroke="#82ca9d" yAxisId="counts" animationDuration={200}/>
+							<Line hide={hide["buildTime"]} dataKey="buildTime" name="Build Time" type="monotone" strokeWidth="2" stroke="#ff7f0e" yAxisId="time"   animationDuration={200}/>
 						</LineChart>
 		}
 		return (
-		<div>
-			<h1><Link to="." title="Go Back to List">{proj}</Link> &gt; Statistics</h1>
-			<div className="buildStatistics">
+		<div className="buildStatistics">
+			<h1>
+				<Link to="." title="Go Back to List">{proj}</Link> &gt; Statistics
+				<span style={{float: "right"}}>
+					<select value={this.state.xType} onChange={this.typeChange}>
+						<option value="commits">Commits</option>
+						<option value="time">Time</option>
+					</select>
+				</span>
+			</h1>
+			<div className="content">
 				<div className="window">
 					{
 						diagram ? 
 						<ResponsiveContainer minHeight={400} width="100%" height="100%">
-   							{diagram}
-   						</ResponsiveContainer>
+							{diagram}
+						</ResponsiveContainer>
 							:
 						<Errors>No statistics available for your project</Errors>}
 				</div>
--- app/src/utils.js
+++ app/src/utils.js
@@ -43,7 +43,8 @@ const Settings = {
 						// proj: {
 						//   line: false
 						// }
-					}
+					},
+					xType: "commits"
 				}
 			};
 		}
